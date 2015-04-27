50c33ade1994115f1bb4c4f20188da20bb2a2379
Puppet Master <hostmaster@teklabs.com>
2015-04-27 18:46:21 +0000

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+class puppetrepo($puppet_version = '3.6.*', $facter_version = '2.1.*') {
+    case $::osfamily {
+        default: { fail("Unsupported platform ${::osfamily}") }
+        'Debian': {
+            apt::source { 'puppetlabs':
+                location => 'http://apt.puppetlabs.com',
+                release => "${::lsbdistcodename}",
+                repos => 'main dependencies',
+                key => '4BD6EC30',
+                key_server => 'pgp.mit.edu',
+            }
+            apt::hold { ['puppet', 'puppet-common']:
+                version => $puppet_version
+            }
+            apt::hold { 'facter':
+                version => $facter_version
+            }
+        }
+        'RedHat': {
+            $script = "${::puppet_vardir}/yum-lock.sh"
+            package { 'yum-plugin-versionlock':
+                ensure => present
+            }
+            ->
+            file { $script:
+                mode => 755,
+                content => template('puppetrepo/yum-lock.sh.erb'),
+            }
+            ~>
+            exec { 'yum-lock':
+                command => $script,
+                refreshonly => true,
+            }
+            cron { 'yum-lock':
+                command => $script,
+                ensure => present,
+                hour => '0',
+                minute => '0',
+            }
+        }
+    }
+}
