51d3a9d87655550814499b8da4b9849a68c86278
Vladimir Kulev <vladimir.kulev@teklabs.com>
2015-08-18 01:52:05 +0300

    Migrate to Puppet 4.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,42 +1,35 @@
-class puppetrepo($puppet_version = '3.7.*', $facter_version = '2.4.*') {
-    case $::osfamily {
-        default: { fail("Unsupported platform ${::osfamily}") }
-        'Debian': {
-            apt::source { 'puppetlabs':
-                location => 'http://apt.puppetlabs.com',
-                release => "${::lsbdistcodename}",
-                repos => 'main dependencies',
-                key => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-                key_server => 'pgp.mit.edu',
-            }
-            apt::hold { ['puppet', 'puppet-common']:
-                version => $puppet_version
-            }
-            apt::hold { 'facter':
-                version => $facter_version
-            }
-        }
-        'RedHat': {
-            $script = "${::puppet_vardir}/yum-lock.sh"
-            package { 'yum-plugin-versionlock':
-                ensure => present
-            }
-            ->
-            file { $script:
-                mode => 755,
-                content => template('puppetrepo/yum-lock.sh.erb'),
-            }
-            ~>
-            exec { 'yum-lock':
-                command => $script,
-                refreshonly => true,
-            }
-            cron { 'yum-lock':
-                command => $script,
-                ensure => present,
-                hour => '3',
-                minute => '45',
-            }
-        }
+class puppetrepo($puppet_agent_version = '1.2.*') {
+  case $::osfamily {
+    default: { fail("Unsupported platform ${::osfamily}") }
+    'Debian': {
+      apt::pin { 'puppet-agent':
+        version  => $puppet_agent_version,
+        priority => 1001,
+      }
     }
+    'RedHat': {
+      $script = "${::puppet_vardir}/yum-lock.sh"
+      ensure_resource('package', 'yum-utils', { 'ensure' => 'present' })
+      ensure_resource('package', 'yum-plugin-versionlock', { 'ensure' => 'present' })
+      file { $script:
+        mode    => '755',
+        content => template('puppetrepo/yum-lock.sh.erb'),
+        require => [
+          Package['yum-utils'],
+          Package['yum-plugin-versionlock'],
+        ],
+      }
+      ~>
+      exec { 'puppet-yum-lock':
+        command     => $script,
+        refreshonly => true,
+      }
+      cron { 'puppet-yum-lock':
+        command => $script,
+        ensure  => present,
+        hour    => '3',
+        minute  => '45',
+      }
+    }
+  }
 }
