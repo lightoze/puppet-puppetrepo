5ad1c80c4faa060a6938ff4fc0d952b83200adfe
Vladimir Kulev <vladimir.kulev@teklabs.com>
2015-08-18 02:04:42 +0300

    Correct package name in install script

--- files/install.sh
+++ files/install.sh
@@ -6,12 +6,12 @@ PUPPET=1.2
 
 if [[ $ID == centos || $ID == rhel ]]; then
     rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${VERSION_ID}.noarch.rpm
-    yum install -y "puppet-$PUPPET.*"
+    yum install -y "puppet-agent-$PUPPET.*"
 elif [[ $ID == debian || $ID == ubuntu ]]; then
     wget -O /tmp/puppet-release.deb https://apt.puppetlabs.com/puppetlabs-release-pc1-$(lsb_release -cs).deb
     dpkg -i /tmp/puppet-release.deb
     apt-get update
-    apt-get install -y "puppet=$PUPPET.*"
+    apt-get install -y "puppet-agent=$PUPPET.*"
 else
     echo "Unsupported OS $PRETTY_NAME"
     exit 1
