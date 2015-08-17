96be1bfafef541d655db9cb75970a8a9aa3aa447
Vladimir Kulev <vladimir.kulev@teklabs.com>
2015-08-18 01:54:21 +0300

    Add install script

---
+++ files/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. /etc/os-release
+
+PUPPET=1.2
+
+if [[ $ID == centos || $ID == rhel ]]; then
+    rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${VERSION_ID}.noarch.rpm
+    yum install -y "puppet-$PUPPET.*"
+elif [[ $ID == debian || $ID == ubuntu ]]; then
+    wget -O /tmp/puppet-release.deb https://apt.puppetlabs.com/puppetlabs-release-pc1-$(lsb_release -cs).deb
+    dpkg -i /tmp/puppet-release.deb
+    apt-get update
+    apt-get install -y "puppet=$PUPPET.*"
+else
+    echo "Unsupported OS $PRETTY_NAME"
+    exit 1
+fi
+
+puppet agent --enable
+echo "Type Puppet server name, followed by [ENTER]:"
+read server
+puppet agent --test --waitforcert 60 --server ${server}
