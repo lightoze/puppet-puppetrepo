d767915301170770d18ba3db8572f0afa1669291
Vladimir Kulev <me@lightoze.net>
2016-04-30 22:17:47 +0300

    Allow configurable server name for install.sh

--- manifests/install_script.pp
+++ manifests/install_script.pp
@@ -1,6 +1,7 @@
 define puppetrepo::install_script(
   $file = $name,
-  $puppet_agent_version = $puppetrepo::puppet_agent_version
+  $puppet_agent_version = $puppetrepo::puppet_agent_version,
+  $servername = $::servername,
 ) {
   file { $file:
     content => template('puppetrepo/install.sh.erb'),
